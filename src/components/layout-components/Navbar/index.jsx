675567dab9cb0e82a22d386234448e7645002d1f
Umang Maheshwari <umang.ladha15@gmail.com>
2023-07-12 16:39:29 +0530

    fix: fixed hamburger menu in responsive

--- src/components/layout-components/Navbar/index.jsx
+++ src/components/layout-components/Navbar/index.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
-import {Link as ReactLink } from "react-router-dom";
+import { Link as ReactLink } from "react-router-dom";
 import { Link } from "react-scroll";
+
 
 const Navbar = () => {
   const navLinks = [
@@ -43,21 +44,61 @@ const Navbar = () => {
   useEffect(() => {
     const sections = document.querySelectorAll("section");
     const handleScroll = () => {
+      var st = window.scrollY;
+      var navbar = $(".ftco_navbar");
+      var sd = $(".js-scroll-wrap");
+      if (st > 150) {
+        if (!navbar.hasClass("scrolled")) {
+          navbar.addClass("scrolled");
+        }
+      }
+      if (st < 150) {
+        if (navbar.hasClass("scrolled")) {
+          navbar.removeClass("scrolled sleep");
+        }
+      }
+      if (st > 350) {
+        if (!navbar.hasClass("awake")) {
+          navbar.addClass("awake");
+        }
+
+        if (sd.length > 0) {
+          sd.addClass("sleep");
+        }
+      }
+      if (st < 350) {
+        if (navbar.hasClass("awake")) {
+          navbar.removeClass("awake");
+          navbar.addClass("sleep");
+        }
+        if (sd.length > 0) {
+          sd.removeClass("sleep");
+        }
+      }
       var current = "";
       sections.forEach((section) => {
-        if (window.scrollY >= section.offsetTop - 160) {
+        if (window.scrollY >= section.offsetTop - 100) {
           current = section.getAttribute("id");
           setCurrentNav(current);
         }
       });
     };
 
-    window.addEventListener("scroll", handleScroll);
+    window.addEventListener("scroll", handleScroll, { passive: true });
 
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
   });
+
+  const handleBurgerMenu = (event) => {
+    event.preventDefault();
+    if ($("#ftco-nav").is(":visible")) {
+      $(this).removeClass("active");
+    } else {
+      $(this).addClass("active");
+    }
+  };
 
   return (
     <nav
@@ -68,19 +109,19 @@ const Navbar = () => {
         <ReactLink className="navbar-brand" to="/">
           Umang
         </ReactLink>
+
         <button
-          className="navbar-toggler js-fh5co-nav-toggle fh5co-nav-toggle"
+          className="navbar-toggler"
           type="button"
           data-toggle="collapse"
-          data-target="#ftco-nav"
-          aria-controls="ftco-nav"
+          data-target="#dd"
+          aria-controls="dd"
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
           <span className="oi oi-menu"></span> Menu
         </button>
-
-        <div className="collapse navbar-collapse" id="ftco-nav">
+        <div className="collapse navbar-collapse" id="dd">
           <ul className="navbar-nav nav ml-auto">
             {navLinks.map(({ id, name }) => (
               <li key={id} className="nav-item">
