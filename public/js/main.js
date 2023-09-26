74828415075d691254ed31f428af806543b1bd59
Umang Maheshwari <umang.ladha15@gmail.com>
2023-09-26 10:43:03 +0530

    minor fixes and changes

--- public/js/main.js
+++ public/js/main.js
@@ -64,7 +64,6 @@
 				$('.number').each(function () {
 					var $this = $(this),
 						num = $this.data('number');
-					console.log(num);
 					$this.animateNumber(
 						{
 							number: num,
--- src/components/layout-components/Sections/Projects/index.jsx
+++ src/components/layout-components/Sections/Projects/index.jsx
@@ -7,6 +7,7 @@ import { useAxios } from "@utils/useFetch";
 import { GET_IMAGE, GET_PROJECTS } from "../../../../constants/ApiConstants";
 import { Link } from "react-router-dom";
 import SectionLoader from "../../../shared-components/SectionLoader";
+import Fallback from '/images/fallback.png'
 
 const ProjectSection = () => {
 
@@ -21,8 +22,6 @@ const ProjectSection = () => {
       return res.data.projects;
     }
   );
-
-  console.log(projects);
 
   return (
     <Section className="ftco-project" id="projects-section" noPb={false}>
@@ -43,14 +42,15 @@ const ProjectSection = () => {
         (
           <div className="row">
             {projects.map((item, index) => (
-              <div className={`col-md-${index % 2 == 0 ? 4 : 8}`}>
+              <div key={item._id} className={`col-md-${index % 2 == 0 ? 4 : 8}`}>
                 <AnimateOnScroll
                   className="project img d-flex justify-content-center align-items-center"
                   style={{
-                    backgroundImage: `url(${handleImage(
+                    backgroundImage: `url(${item.images.length?handleImage(
                       GET_IMAGE,
                       item.images[0].name
-                    )})`,
+                    ):Fallback})`,
+                    backgroundPosition: `${!item.images.length && 'center'}`
                   }}
                 >
                   <Link target="_blank" className="w-100" to={item.src}>
@@ -65,94 +65,7 @@ const ProjectSection = () => {
                 </AnimateOnScroll>
               </div>
             ))}
-            {/* <div className="col-md-4">
-            <AnimateOnScroll
-              className="project img d-flex justify-content-center align-items-center"
-              style={{ backgroundImage: `url(${img4})` }}
-            >
-              <div className="overlay"></div>
-              <div className="text text-center p-4">
-                <h3>
-                  <a href="#">Branding &amp; Illustration Design</a>
-                </h3>
-                <span>Web Design</span>
-              </div>
-            </AnimateOnScroll>
-          </div>
-          <div className="col-md-8">
-            <AnimateOnScroll
-              className="project img d-flex justify-content-center align-items-center"
-              style={{ backgroundImage: `url(${img5})` }}
-            >
-              <div className="overlay"></div>
-              <div className="text text-center p-4">
-                <h3>
-                  <a href="#">Branding &amp; Illustration Design</a>
-                </h3>
-                <span>Web Design</span>
-              </div>
-            </AnimateOnScroll>
-          </div>
-
-          <div className="col-md-8">
-            <AnimateOnScroll
-              className="project img ftco-animate d-flex justify-content-center align-items-center"
-              style={{ backgroundImage: `url(${img1})` }}
-            >
-              <div className="overlay"></div>
-              <div className="text text-center p-4">
-                <h3>
-                  <a href="#">Branding &amp; Illustration Design</a>
-                </h3>
-                <span>Web Design</span>
-              </div>
-            </AnimateOnScroll>
-
-            <AnimateOnScroll
-              className="project img d-flex justify-content-center align-items-center"
-              style={{ backgroundImage: `url(${img6})` }}
-            >
-              <div className="overlay"></div>
-              <div className="text text-center p-4">
-                <h3>
-                  <a href="#">Branding &amp; Illustration Design</a>
-                </h3>
-                <span>Web Design</span>
-              </div>
-            </AnimateOnScroll>
-          </div>
-          <div className="col-md-4">
-            <div className="row">
-              <div className="col-md-12">
-                <AnimateOnScroll
-                  className="project img d-flex justify-content-center align-items-center"
-                  style={{ backgroundImage: `url(${img2})` }}
-                >
-                  <div className="overlay"></div>
-                  <div className="text text-center p-4">
-                    <h3>
-                      <a href="#">Branding &amp; Illustration Design</a>
-                    </h3>
-                    <span>Web Design</span>
-                  </div>
-                </AnimateOnScroll>
-              </div>
-              <div className="col-md-12">
-                <AnimateOnScroll
-                  className="project img d-flex justify-content-center align-items-center"
-                  style={{ backgroundImage: `url(${img3})` }}
-                >
-                  <div className="overlay"></div>
-                  <div className="text text-center p-4">
-                    <h3>
-                      <a href="#">Branding &amp; Illustration Design</a>
-                    </h3>
-                    <span>Web Design</span>
-                  </div>
-                </AnimateOnScroll>
-              </div>
-            </div>
-          </div> */}
+            
           </div>
         )}
       </div>
